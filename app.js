ad877f4fa8e816f20d254d555f25560caa9f7d5f
Ahmad Ibrahim <ahmadaburabee3@gmail.com>
2022-01-13 13:50:01 -0700

    Updated program to match css

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use("/static", express.static("public"));
+
+// Setting pug as our view engine
+app.set("view engine", "pug"); // defaults to views folder for templates
+
+const mainRoutes = require("./routes/index");
+const cardRoutes = require("./routes/cards");
+
+app.use(mainRoutes);
+app.use("/cards", cardRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.status(err.status);
+  res.render("error");
 });
 
 // This sets our app to be accessed at port 3000
